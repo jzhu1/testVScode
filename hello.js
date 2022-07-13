9fa47a43dffae1a56270e55b5588bbfbf8e62719
jzhu1 <21694479+jzhu1@users.noreply.github.com>
2022-07-13 18:29:11 -0400

    domo

--- hello.js
+++ hello.js
@@ -1,7 +1,6 @@
-let message: string = "Hello TypeScript!";
+var message = "Hello TypeScript!";
 //create a new eading 1 element
-let heading = document.createElement('h1');
+var heading = document.createElement('h1');
 heading.textContent = message;
 //add the heading the document
 document.body.appendChild(heading);
-
